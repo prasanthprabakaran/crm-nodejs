3f1cc44d6d82554055d2ab4ab300af35240bc1cf
prasanthprabakaran <iprasanthpraba@gmail.com>
2022-12-29 13:23:26 +0530

    model

--- models/User.js
+++ models/User.js
@@ -4,13 +4,11 @@ import bcrypt from 'bcrypt'
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: [true, "Please provide username"],
+        required: true
     },
     password: {
         type: String,
-        required: [true, "Please add a password"],
-        minlength: 6,
-        select: false,
+        required: true
     },
     roles: {
         type: [String],
@@ -29,21 +27,3 @@ UserSchema.methods.matchPassword = async function (password) {
 const User = mongoose.model("User",UserSchema);
 
 export default User;
-
-    // firstname: {
-    //     type: String,
-    //     required: [true, "Please provide username"],
-    // },
-    // lastname: {
-    //     type: String,
-    //     required: [true, "Please provide username"],
-    // },
-    // email:{
-    //     type: String,
-    //     required: [true, "Please provide email address"],
-    //     unique: true,
-    //     match: [
-    //         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    //         "Please provide a valid email",
-    //       ],
-    // },
